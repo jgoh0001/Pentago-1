76c0213a38998f2bf7bff1ad2da2a7f8f55fd2ef
jgoh0001 <148250798+jgoh0001@users.noreply.github.com>
2023-10-17 23:07:03 +0800

    Add files via upload

---
+++ pentago (1).py
@@ -0,0 +1,30 @@
+import numpy as np
+import random
+
+def display_board(board):
+    # implement your function here
+    pass
+    
+def check_victory(board,turn,rot):
+    # implement your function here
+    return -1
+
+def apply_move(board,turn,row,col,rot):
+    # implement your function here
+    return board
+    
+def check_move(board,row,col):    
+    # implement your function here
+    return False
+
+def computer_move(board,turn,level):
+    # implement your function here
+    return (0,0,0)
+
+def menu():  
+    # implement your function here
+    pass
+
+if __name__ == "__main__":
+    menu()
+    
---
+++ project draft.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 12 16:47:40 2023
+
+@author: janeg
+"""
+
+import numpy as np
+game_board = np.zeros([6,6]) #since the board is a 6 by 6 board, [6,6] need to put in brackets to make it a list, each empty blank is represented as a 0
+print(game_board)
